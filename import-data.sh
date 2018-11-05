4fdc1125ddbf3d3a63469ba9d69711b16123576b
sandy1480 <43827905+sandy1480@users.noreply.github.com>
2018-11-05 22:31:57 +0000

    Update import-data.sh

--- import-data.sh
+++ import-data.sh
@@ -2,7 +2,7 @@
 sleep 90s
 
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password -d master -i setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P S@ndy1480 -d master -i setup.sql
 
 #import the data from the csv file
-/opt/mssql-tools/bin/bcp DemoData.dbo.Employee in "/usr/src/app/Employee.csv" -c -t',' -S localhost -U sa -P Password
+/opt/mssql-tools/bin/bcp DemoData.dbo.Employee in "/usr/src/app/Employee.csv" -c -t',' -S localhost -U sa -P S@ndy1480
