c59087b703169af05c7d0670c0b21ad8b46f667e
sandy1480 <43827905+sandy1480@users.noreply.github.com>
2018-11-05 20:08:57 +0000

    Create import-data.sh

---
+++ import-data.sh
@@ -0,0 +1,8 @@
+#wait for the SQL Server to come up
+sleep 90s
+
+#run the setup script to create the DB and the schema in the DB
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password -d master -i setup.sql
+
+#import the data from the csv file
+/opt/mssql-tools/bin/bcp DemoData.dbo.Employee in "/usr/src/app/Employee.csv" -c -t',' -S localhost -U sa -P Password
